7a89c7abd1f83124ec096d888ef2098b96ba67eb
Manuel Montenegro <manuel@nucypher.com>
2022-06-08 10:49:37 +0200

    Avoid non-null assertions operators
    The use of non-null assertions operators on Typescript is not
    recommended whenever possible. Avoiding the use of them, let the
    compiler do the job of statically checking the code.

--- src/staking.ts
+++ src/staking.ts
@@ -232,10 +232,12 @@ export function handleUnstaked(event: Unstaked): void {
   const epochStakes = populateNewEpochStakes(lastEpoch.stakes)
   const epochStakeId = getEpochStakeId(stakingProvider)
   const epochStake = EpochStake.load(epochStakeId)
-  epochStake!.amount = epochStake!.amount.minus(amount)
-  epochStake!.save()
-  if (epochStake!.amount.isZero()) {
-    epochStakes.splice(epochStakes.indexOf(epochStakeId), 1)
+  if (epochStake) {
+    epochStake.amount = epochStake.amount.minus(amount)
+    epochStake.save()
+    if (epochStake.amount.isZero()) {
+      epochStakes.splice(epochStakes.indexOf(epochStakeId), 1)
+    }
   }
 
   const epoch = new Epoch(getEpochCount().toString())
