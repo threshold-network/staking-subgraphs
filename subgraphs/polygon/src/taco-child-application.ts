88893b27d079c783a6c5daddb23f57b0e9ac9ee1
Manuel Montenegro <manuel@nucypher.com>
2024-01-29 13:04:21 +0100

    Add Threshold Staking subgraph for Polygon

---
+++ subgraphs/polygon/src/taco-child-application.ts
@@ -0,0 +1,31 @@
+import {
+  OperatorConfirmed as OperatorConfirmedEvent,
+  OperatorUpdated as OperatorUpdatedEvent,
+} from "../generated/TACoChildApplication/TACoChildApplication"
+import { TACoOperator } from "../generated/schema"
+
+export function handleOperatorConfirmed(event: OperatorConfirmedEvent): void {
+  const stakingProvider = event.params.stakingProvider
+  const operator = event.params.operator
+  const timestamp = event.block.timestamp
+
+  let tacoOperator = TACoOperator.load(stakingProvider.toHexString())
+  if (!tacoOperator) {
+    tacoOperator = new TACoOperator(stakingProvider.toHexString())
+    tacoOperator.confirmedTimestampFirstOperator = timestamp
+  }
+  tacoOperator.operator = operator
+  tacoOperator.confirmedTimestamp = timestamp
+  tacoOperator.active = true
+  tacoOperator.save()
+}
+
+export function handleOperatorUpdated(event: OperatorUpdatedEvent): void {
+  const stakingProvider = event.params.stakingProvider
+
+  const tacoOperator = TACoOperator.load(stakingProvider.toHexString())
+  if (tacoOperator) {
+    tacoOperator.active = false
+    tacoOperator.save()
+  }
+}
